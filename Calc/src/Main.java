2d13cabb0e5d8766d20aeb1fc84e4069dc4db938
sakarpey <139759975+sakarpey@users.noreply.github.com>
2023-08-26 17:26:00 +0300

    Update Main.java
    test comment

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -9,7 +9,7 @@ public class Main {                             // КЛАСС
         String itog=calc(input);            // Вызываем метод calc для решения
         System.out.print("Указанное вами, выражение равно " + itog);            // Выводим результат 'itog'
     }
-//    public static String calc(String input) throws Exception {
+   public static String calc(String input) throws Exception {
 //        String[] romVal = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
 //                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
 //                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
